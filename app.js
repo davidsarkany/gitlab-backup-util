510cc9b5538617118e73f3076ef6f33a843d2789
Dávid Sárkány <mail@sarkanydavid.com>
2019-05-10 14:19:21 +0200

    Refactor

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 
 require('dotenv').config();
 const request = require('request-promise');
-const git = require("nodegit");
+const git = require('nodegit');
 const server = process.env.SERVER;
-const serverProtocol = server.split("/")[0]+"//";
+const serverProtocol = `${server.split('/')[0]}//`;
 const token = process.env.TOKEN;
 const backupFolder = process.env.BACKUP_FOLDER;
 
@@ -25,23 +25,23 @@ const generateAuthorizedRepoUrl = (url) => {
 };
 
 (async () => {
-    const repos = [];
+    const repositories = [];
     const groups = await authorizedGetRequest(`${server}/api/v4/groups`);
     for(const group of groups){
         let groupProjects = await authorizedGetRequest(`${server}/api/v4/groups/${group.id}/projects`);
         for(const groupProject of groupProjects){
-            repos.push({name:groupProject.name, path: groupProject.path, url: groupProject.http_url_to_repo});
+            repositories.push({name:groupProject.name, path: groupProject.path, url: groupProject.http_url_to_repo});
         }
     }
 
     const user = await authorizedGetRequest(`${server}/api/v4/user`);
     const userProjects = await authorizedGetRequest(`${server}/api/v4/users/${user.id}/projects`);
     for(const userProject of userProjects){
-        repos.push({name:userProject.name, path: userProject.path, url: userProject.http_url_to_repo});
+        repositories.push({name:userProject.name, path: userProject.path, url: userProject.http_url_to_repo});
     }
 
-    for(const repo of repos){
-        await git.Clone(generateAuthorizedRepoUrl(repo.url), backupFolder + '/' + repo.path);
-        console.info(repo.name);
+    for(const repository of repositories){
+        await git.Clone(generateAuthorizedRepoUrl(repository.url), `${backupFolder}/${repository.path}`);
+        console.info(repository.name);
     }
 })();
